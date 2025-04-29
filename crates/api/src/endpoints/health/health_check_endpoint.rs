ad2a132b4a05341b63d185d8078f009c6c940876
Not a cow <kly3333aswd@gmail.com>
2025-04-28 20:03:22 -0700

    feat: hi

--- crates/api/src/endpoints/health/health_check_endpoint.rs
+++ crates/api/src/endpoints/health/health_check_endpoint.rs
@@ -1,16 +1,8 @@
+use axum::Router;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get;
-use axum::{Json, Router};
-use serde::Serialize;
 
 pub fn health_check() -> Router { Router::new().route("/health", get(handle_request)) }
 
-#[derive(Serialize)]
-struct Response {
-	success: bool,
-}
-
-async fn handle_request() -> impl IntoResponse {
-	(StatusCode::OK, Json(Response { success: true }))
-}
+async fn handle_request() -> impl IntoResponse { (StatusCode::OK, "hi") }
